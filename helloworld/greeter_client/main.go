1c0656d311af8024e1ec85367ef60ee48ae0ee9c
oneoneonepig <peihuachu1112@gmail.com>
2019-05-27 00:22:20 +0800

    Create main.go

---
+++ helloworld/greeter_client/main.go
@@ -0,0 +1,77 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// Package main implements a client for Greeter service.
+package main
+
+import (
+        "context"
+        "flag"
+        "log"
+        "os"
+        "time"
+
+        "google.golang.org/grpc"
+        pb "google.golang.org/grpc/examples/helloworld/helloworld"
+)
+
+/*
+const (
+         address     = "localhost:32123"
+        defaultName = "world"
+)
+*/
+
+var (
+        host    string
+        port    string
+        message string
+)
+
+// Initialize listen port and address
+func init() {
+        flag.StringVar(&host, "host", "0.0.0.0", "help message for host")
+        flag.StringVar(&port, "port", "32123", "help message for port")
+        flag.StringVar(&message, "message", "world", "help message for message")
+}
+
+func main() {
+        flag.Parse()
+
+        // Set up a connection to the server.
+        conn, err := grpc.Dial(host+":"+port, grpc.WithInsecure())
+        if err != nil {
+                log.Fatalf("did not connect: %v", err)
+        }
+        defer conn.Close()
+        c := pb.NewGreeterClient(conn)
+
+        name := message
+        hostname, err := os.Hostname()
+        if err != nil {
+                log.Fatalf("could not detect hostname: %v", err)
+        }
+
+        ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+        defer cancel()
+        r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name, Hostname: hostname})
+        if err != nil {
+                log.Fatalf("could not greet: %v", err)
+        }
+        log.Printf("Reply: %s from %s", r.Message, r.Hostname)
+}
