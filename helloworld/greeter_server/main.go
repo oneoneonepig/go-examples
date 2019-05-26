fca60304ae69b0e68f52523ca0f7a1fb589b1e14
oneoneonepig <peihuachu1112@gmail.com>
2019-05-27 00:21:51 +0800

    Create main.go

---
+++ helloworld/greeter_server/main.go
@@ -0,0 +1,70 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+//go:generate protoc -I ../helloworld --go_out=plugins=grpc:../helloworld ../helloworld/helloworld.proto
+
+// Package main implements a server for Greeter service.
+package main
+
+import (
+        "context"
+        "flag"
+        "log"
+        "net"
+        "os"
+
+        "google.golang.org/grpc"
+        pb "google.golang.org/grpc/examples/helloworld/helloworld"
+)
+
+var (
+        host string
+        port string
+)
+
+// Initialize listen port and address
+func init() {
+        flag.StringVar(&host, "host", "0.0.0.0", "help message for host")
+        flag.StringVar(&port, "port", "32123", "help message for port")
+}
+
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+        log.Printf("Received: %v from %v", in.Name, in.Hostname)
+        hostname, err := os.Hostname()
+        if err != nil {
+                log.Fatalf("could not detect hostname: %v", err)
+        }
+        return &pb.HelloReply{Message: "Hello " + in.Name, Hostname: hostname}, nil
+}
+
+func main() {
+        flag.Parse()
+        lis, err := net.Listen("tcp", host+":"+port)
+        if err != nil {
+                log.Fatalf("failed to listen: %v", err)
+        }
+        s := grpc.NewServer()
+        pb.RegisterGreeterServer(s, &server{})
+        if err := s.Serve(lis); err != nil {
+                log.Fatalf("failed to serve: %v", err)
+        }
+}
