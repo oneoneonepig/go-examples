abf6e96fc840d7efef64de9b0c3fcdeaa505d795
oneoneonepig <peihuachu1112@gmail.com>
2019-05-29 00:32:01 +0800

    Update main.go

--- helloworld/greeter_client/main.go
+++ helloworld/greeter_client/main.go
@@ -1,22 +1,3 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-// Package main implements a client for Greeter service.
 package main
 
 import (
@@ -45,7 +26,6 @@ var (
         message		string
 )
 
-// Initialize listen port and address
 func init() {
         flag.StringVar(&host, "host", "0.0.0.0", "Target address")
         flag.StringVar(&port, "port", "3000", "Target port")
@@ -56,13 +36,13 @@ func init() {
 
 func main() {
 
+	conn, err := grpc.Dial(host+":"+port, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	        // Set up a connection to the server.
-	        conn, err := grpc.Dial(host+":"+port, grpc.WithInsecure())
-	        if err != nil {
-	                log.Fatalf("did not connect: %v", err)
-	        }
-	        defer conn.Close()
 	        c := pb.NewGreeterClient(conn)
 
 	        name := message
@@ -85,9 +65,5 @@ func main() {
 		json.NewEncoder(w).Encode(fr)
 	})
 
-	err := http.ListenAndServe(":"+listenPort, nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-		os.Exit(1)
-	}
+	log.Fatal(http.ListenAndServe(":"+listenPort, nil))
 }
